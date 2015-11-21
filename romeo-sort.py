2656ad5ef9d01e942889f21942c5bb211118be06
Alan Williams <alanwill81@gmail.com>
2015-11-21 00:33:47 -0800

    working code

--- romeo-sort.py
+++ romeo-sort.py
@@ -2,4 +2,12 @@ fname = raw_input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-print line.rstrip()
+   # print line.rstrip()
+    words = line.split()
+    count = 0
+    for uniqueword in words:
+        if uniqueword not in lst:
+            lst.append(uniqueword)
+        continue
+lst.sort()
+print lst
