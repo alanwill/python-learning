a07659884bd6defae5db954095c49d7fb54a1547
Alan Williams <alanwill81@gmail.com>
2015-11-23 22:58:28 -0800

    Added rstrip to clean the code up some

--- email-extraction.py
+++ email-extraction.py
@@ -7,6 +7,7 @@ count = 0
 # Loop through the file and ignore any line that does NOT start with 'From '
 for line in fh:
     if not line.startswith('From ') : continue
+    line = line.rstrip()
     parsed = line.split()
     print parsed[1]
     count = count + 1
